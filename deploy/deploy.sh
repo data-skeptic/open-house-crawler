c82aed9e2a7685887c9a8d2b117680ef0ed9185e
Kyle Polich <kylepolich@gmail.com>
2016-10-24 09:34:08 -0700

    working on deploy

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -25,6 +25,7 @@ echo api_user=$api_user > api_creds.txt
 echo api_passwd=$api_passwd >> api_creds.txt
 echo api_base_url=$api_baseurl >> api_creds.txt
 zip -r ~/deps.zip api_creds.txt
+rm -f api_creds.conf
 
 cd /usr
 
